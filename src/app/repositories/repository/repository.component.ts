8021067694d997afa370d687e226b2c9a023e4eb
OlegGre4ka <gre4kae@ukr.net>
2018-12-15 00:37:29 +0300

    get and set likes in/from localeStorage -RepositoryComponent

--- src/app/repositories/repository/repository.component.ts
+++ src/app/repositories/repository/repository.component.ts
@@ -1,30 +1,62 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Repos } from 'src/app/shared/models/Repos';
+import { Component, OnInit, Input } from "@angular/core";
+import { Repos } from "src/app/shared/models/Repos";
 @Component({
-  selector: 'app-repository',
-  templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.scss']
+  selector: "app-repository",
+  templateUrl: "./repository.component.html",
+  styleUrls: ["./repository.component.scss"]
 })
 export class RepositoryComponent implements OnInit {
   // tslint:disable-next-line:no-input-rename
-  @Input('reposItem') repos: Repos;
-  @Input('keyword') keyword: string;
+  @Input("reposItem") repos: Repos;
+  @Input("keyword") keyword: string;
   id;
-  like = true;
+  like = 0;
   isPressed = false;
-  getLike;
+  countLike;
+  // idLocalStorageArray = [];
+  setID;
+  // setCountLike;
+  // likeObj = {};
+  // objKeysInStorage = [];
+  getID;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.repos.id, 'repos.id');
 
+    this.getLikeFromLocaleStorage(this.repos.id);
+
+    // const keys = Object.keys(window.localStorage);
+    // // this.likedRepos(this.id, keys);
+    // console.log(keys);
+    // this.getID =  keys.filter(key => key === '13261856');
+    // this.countLike = window.localStorage.getItem(this.getID);
+    // console.log(this.getID, this.countLike , 'IDget');
+
+    // if (
+
+    // ) {
+
+    // }
+  }
+  getLikeFromLocaleStorage(id) {
+    const keys = Object.keys(window.localStorage);
+    // this.likedRepos(this.id, keys);
+    console.log(keys);
+    this.getID = keys.filter(key => key === id.toString());
+    this.countLike = window.localStorage.getItem(this.getID);
+    console.log(this.getID, this.countLike, "IDget");
+    if ( !!this.countLike === true) {
+    this.isPressed = true;
+    }
+  }
   likedRepos(id) {
-    if (this.isPressed === false) {
+    // this.countLike =  (++this.like).toString();
+
+    // window.localStorage.setItem(id.toString(), this.countLike);
+    window.localStorage.setItem(id.toString(), (++this.like).toString());
+
       this.isPressed = true;
 
-      window.localStorage.setItem(id.toString(), this.like.toString());
-    } else {
-      this.isPressed = false;
-      window.localStorage.removeItem(id.toString());
-    }
   }
 }
